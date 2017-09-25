940cc57d9a8f1006ded42bceb38a1e3e5d2226bc
Ryan Walberg <ryan.walberg@gmail.com>
2017-09-25 03:12:01 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,16 @@
 # code your #position_taken? method here!
+
+def position_taken?(board, input)
+
+end
+
+def input_to_index(user_input)
+  (user_input.to_i) - 1
+end
+
+if position_taken?(board, index) == " "
+  return false
+#if board[index] == " "
+#  puts false
+#else
+#  puts true
